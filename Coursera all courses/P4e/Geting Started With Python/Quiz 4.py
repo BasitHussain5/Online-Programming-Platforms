6c6f2b1c46ae31e2473059d277026f5f2b0d53ee
Basit Hussain <87776461+BasitHussain5@users.noreply.github.com>
2022-02-01 12:16:49 +0600

    Add files via upload

---
+++ Coursera all courses/P4e/Geting Started With Python/Quiz 4.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Question 1
+get_ipython().set_next_input('Which of the following is a comment in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+* This is a test
+(/, This, is, a, test)
+# This is a test
+*(This, is, a, test, */)
+
+
+Answer: # This is a test
+
+Question 2
+get_ipython().set_next_input('What does the following code print out');get_ipython().run_line_magic('pinfo', 'out')
+
+print "123" + "abc"
+This is a syntax error because you cannot add strings
+hello world
+123abc
+123+abc 
+
+Answer: 123abc
+
+Question 3
+get_ipython().set_next_input('Which of the following is a bad Python variable name');get_ipython().run_line_magic('pinfo', 'name')
+
+spam23
+SPAM23
+23spam
+spam_23
+
+Answer: 23spam
+
+Question 4
+get_ipython().set_next_input('Which of the following is not a Python reserved word');get_ipython().run_line_magic('pinfo', 'word')
+spam
+else
+for
+if
+
+Answer: spam
+
+Question 5
+get_ipython().set_next_input('What does the following statement do');get_ipython().run_line_magic('pinfo', 'do')
+x = x + 2
+
+Exit the program
+This would fail as it is a syntax error
+Increase the speed of the program by a factor of 2
+Retrieve the current value for x, add two to it and put the sum back into x
+
+Answer: Retrieve the current value for x, add two to it and put the sum back into x
+
+Question 6
+get_ipython().set_next_input('Which of the following elements of a mathematical expression in Python is evaluated first');get_ipython().run_line_magic('pinfo', 'first')
+
+Parenthesis ( )
+Subtraction -
+Multiplication *
+Addition +
+
+Answer: Parenthesis ( )
+
+Question 7
+What is the value of the following expression
+42 % 10
+
+Hint - the "%" is the remainder operator
+0.42
+420
+1042
+2
+
+Answer: 2
+
+
+Question 8
+What is the value in x after the following statement executes:
+x = 1 + 2 * 3 - 8 / 4
+
+5
+4
+15
+8
+
+Answer: 5
+
+Question 9
+What value be in the variable x when the following statement is executed
+
+x = int(98.6)
+100
+99
+98
+6
+
+Answer: 98
+
+Question 10
+get_ipython().set_next_input('What does the Python raw_input() function do');get_ipython().run_line_magic('pinfo', 'do')
+
+Connect to the network and retrieve a web page.
+Take a screen shot from an area of the screen
+Pause the program and read data from the user
+Read the memory of the running program
+
+Answer: Pause the program and read data from the user 
+
