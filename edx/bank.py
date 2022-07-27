fe368e8f13f9957db1b161a6d69912e30a279738
Basit Hussain <87776461+BasitHussain5@users.noreply.github.com>
2022-07-27 18:07:31 +0600

    Create bank.py

---
+++ edx/bank.py
@@ -0,0 +1,11 @@
+ans = input("Greetings: ")
+new_ans = ans.lower().strip()
+
+if "hello" in new_ans:
+    print("$0")
+    
+elif "h" == new_ans[0]:
+    print("$20")
+    
+else:
+    print("$100")
