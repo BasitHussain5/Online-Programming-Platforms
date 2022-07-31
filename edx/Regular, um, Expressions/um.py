b6d1d3bfaa069ba295c88978609a2536e56d4f78
Basit Hussain <87776461+BasitHussain5@users.noreply.github.com>
2022-07-31 09:35:28 +0600

    Create um.py

---
+++ edx/Regular, um, Expressions/um.py
@@ -0,0 +1,15 @@
+import re
+import sys
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    um_list = re.findall(r"\b\w*um\w*", s, re.IGNORECASE)
+    return len(um_list)
+
+
+if __name__ == "__main__":
+    main()
