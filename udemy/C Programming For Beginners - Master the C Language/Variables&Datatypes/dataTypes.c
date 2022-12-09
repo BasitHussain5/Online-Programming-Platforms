de759a09a0e632f21f728bf84b860c5ce581223f
Basit Hussain <87776461+BasitHussain5@users.noreply.github.com>
2022-12-09 18:23:25 +0600

    Add files via upload

---
+++ udemy/C Programming For Beginners - Master the C Language/Variables&Datatypes/dataTypes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int d = 12;
+    float f = 14.0;
+    double e = 1.2e+11;
+    _Bool i = 0;
+    char c = 'A';
+
+
+   printf("The value of integer is: %i\n", d);
+   printf("The value of float is: %f\n", f);
+   printf("The value of double is: %e\n", e);
+   printf("The value of double is: %g\n", e);
+   printf("The value of bool is: %i\n", i);
+   printf("The value of chararcter is: %c\n", c);
+
+    return 0;
+}
